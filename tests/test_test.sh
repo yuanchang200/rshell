41a8d17a58742419c7af1eff5e8042a09d73fdcc
U-Chang\Yuan <18635516329@163.com>
2016-11-15 23:28:07 -0800

    updated tests

---
+++ tests/test_test.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+test yuan
+test -e yuan
+test -d yuan
+test -f yuan
+[ yuan ]
+[ -e yuan ]
+[ -d yuan ]
+[ -f yuan ]
+test yuan chang
+test yuan ]
+test -e yuan chang
+test -d yuan chang
+test -f yuan chang
+test -e yuan ]
+test -d yuan ]
+test -f yuan ]
+test -g yuan
+test -g
+test -h yuan
+test -h
+test -g yuan chang
+test -g yuan ]
+[yuan]
+[-e yuan]
+[-d yuan]
+[-f yuan]
+[yuan 
+[ yuan
+[-e yuan
+[-d yuan
+[-f yuan
+
