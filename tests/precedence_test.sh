4addc0d83c8e897fe1a8dafd52c65f18b712f1af
U-Chang\Yuan <18635516329@163.com>
2016-11-18 17:55:19 -0800

    updated tests

--- tests/precedence_test.sh
+++ tests/precedence_test.sh
@@ -10,4 +10,14 @@ echo yuan || (echo chang && echo u)
 (test yuan && (echo yuan || echo o)) && ls -a
 ( test yuan && echo yuan ) || ( echo y && echo c )
 ( test yuan && echo yuan) || (echo y && echo c )
+test yuan && (echo yuan; echo chang) && echo o
+(test chang || echo o) && (test yuan || echo u)
+(test yuan; echo u) && (exit; echo i)
+test yuan; (echo yuan || echo yuan); echo i
+(((echo yuan && test yuan) || test o) && echo o) && exit
+(((echo yuan || test chang); test o) && echo p) || exit
+(((echo yuan; test yuan); echo i) || echo p); echo i
+echo yuan || (test yuan && (ls -a || exit;(echo u && echo i)))
+test yuan && (test u || ls -a || (exit && (echo p; echo i)))
+echo yuan && (test yuan; (echo u || echo y) || (echo yuan; echo u)) && exit
 
